5807e33c0ce08e650edd5b3590dee83526cbb449
Daniel Voicu <daniel.voicu.dev@gmail.com>
2024-01-29 17:15:50 +0200

    update add to cart component

--- src/comm-add-to-cart.js
+++ src/comm-add-to-cart.js
@@ -1,7 +1,10 @@
 ﻿import { LitElement, css, html } from 'lit'
-
+import {when} from 'lit/directives/when.js';
+import {CommQuantity} from "./comm-quantity.js";
+import {ifDefined} from "lit/directives/if-defined.js";
 export class CommAddToCart extends LitElement {
-  
+  static formAssociated = true;
+  static shadowRootOptions = {...LitElement.shadowRootOptions, delegatesFocus: true};
   static properties = {
     value: {type: Number},
     disabled: {type: Boolean,attribute: "disabled"},
@@ -10,7 +13,27 @@ export class CommAddToCart extends LitElement {
     step: {type: Number},
     type: {type: String},
     stepper: {type: Boolean, attribute: "stepper"},
-    readonly: {type: Boolean}
-    render()
+    readonly: {type: Boolean},
+    quantityEnabled: {type: Boolean, attribute: "quantity-enabled"},
   };
+  
+  constructor () {
+    super()
+    this.value = 1;
+    this.type = "number";   
+    this.stepper = false;
+  }
+
+  render() {
+    return html`
+      ${when(this.quantityEnabled, () => html`
+        <comm-quantity value=${this.value} min=${ifDefined(this.min)} max=${ifDefined(this.max)} step=${ifDefined(this.step)} type=${ifDefined(this.type)} stepper=${ifDefined(this.stepper)} ?disabled=${this.disabled} ?readonly=${this.readonly} @change=${this._onChange}></comm-quantity>
+      `, ()=> html`
+      <button type="button">
+        <span>add to cart</span>
+      </button>`)}
+    `;
+  }
 }
+
+customElements.define('comm-add-to-cart', CommAddToCart);
